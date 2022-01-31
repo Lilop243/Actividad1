cae1edf9ca4f20462188ed607eb089a3beacc019
lida lopez <linda.lucia15@hotmail.com>
2022-01-31 18:10:13 -0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {} from 'bootstrap'
+export const Card = ({ personaje }) => {
+	return (
+		<section className='personaje'>
+			<div className='personaje-header'>
+				<div className='estado'>
+				</div>
+			</div>
+
+			<div className='personaje-body '>
+				<figure>
+					<img src={personaje.image} class="img-thumbnail" alt={personaje.name} />
+				</figure>
+
+         <div>
+                <h2>{personaje.name}</h2>
+        </div>
+				
+			</div>
+		</section>
+	)
+}
